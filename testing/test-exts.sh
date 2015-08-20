cb47b5e44700de12552af6778d17a322f2e545a9
Lucas Kramer <krame505@umn.edu>
2015-08-20 11:33:35 -0500

    Improved testing functionality

--- testing/test-exts.sh
+++ testing/test-exts.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # An exceedingly crude and simple mechanism to test various extensions.
-# This script just calls individual testing scripts in the extendion
-# directories.
+# This script just runs make test in each of the extension directories.
 
 cd ../
 
 cd extensions/conditional-tables/
-bash $@ ./test.sh
+#bash $@ ./make tests
 cd ../../
 
 cd extensions/algebraic-data-types/
-bash $@ ./test.sh
+bash $@ ./make tests
 cd ../../
 
 cd extensions/gc/
-bash $@ ./test.sh
+bash $@ ./make tests
+cd ../../
+
+cd extensions/gc-algebraic-data-types/
+bash $@ ./make tests
 cd ../../
 
 cd extensions/closure/
-bash $@ ./test.sh
+bash $@ ./make tests
 cd ../../
 
