53250a7e8d824d7b15a9680fa6c4cee69f48c887
Lucas Kramer <krame505@umn.edu>
2015-08-20 13:43:50 -0500

    gc sets up paths for MELT computers

--- extensions/gc/init_build.sh
+++ extensions/gc/init_build.sh
@@ -3,3 +3,8 @@
 export ABLEC_SOURCE+=" $ABLEC_PATH/extensions/gc/edu.umn.cs.melt.exts.ableC.gc/**/*"
 export SILVER_INCLUDES+=" -I $ABLEC_PATH/extensions/gc"
 export LDLIBS+=" -lgc"
+
+if [ -f /project/melt/Software/setup_paths.sh ]
+then
+    source /project/melt/Software/setup_paths.sh
+fi
