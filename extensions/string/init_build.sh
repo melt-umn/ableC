cdfe826657bde948871e03ab89b9739d4a3ce0b5
Lucas Kramer <krame505@umn.edu>
2016-01-22 14:59:18 -0600

    Bug fix for string extension build parameters

--- extensions/string/init_build.sh
+++ extensions/string/init_build.sh
@@ -4,5 +4,5 @@ source $ABLEC_PATH/extensions/gc/init_build.sh # Don't really need everything, b
 
 export ABLEC_SOURCE+=" $ABLEC_PATH/extensions/string/edu.umn.cs.melt.exts.ableC.string/**/*"
 export SILVER_INCLUDES+=" -I $ABLEC_PATH/extensions/string"
-export TRANSLATE_DEPENDS+="$ABLEC_PATH/extensions/string/include/*"
+export TRANSLATE_DEPENDS+=" $ABLEC_PATH/extensions/string/include/*"
 export CPPFLAGS+=" -I$ABLEC_PATH/extensions/string/include"
