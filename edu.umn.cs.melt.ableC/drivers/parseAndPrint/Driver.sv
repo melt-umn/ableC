f0643aeab996f2da2e8b98bf1117e1ede95945cc
Lucas Kramer <krame505@umn.edu>
2016-04-12 13:34:10 -0500

    Rewritten driver using monads

--- edu.umn.cs.melt.ableC/drivers/parseAndPrint/Driver.sv
+++ edu.umn.cs.melt.ableC/drivers/parseAndPrint/Driver.sv
@@ -5,6 +5,7 @@ imports edu:umn:cs:melt:ableC:abstractsyntax as abs;
 
 imports silver:langutil;
 imports silver:langutil:pp;
+imports core:monad;
 
 import edu:umn:cs:melt:ableC:abstractsyntax:env ; --only env, emptyEnv;
 
@@ -22,76 +23,91 @@ IOVal<Integer> ::= args::[String] ioIn::IO
   local cppArgs :: [String] = partitionedArgs.snd;
   local xcArgs :: [String] = partitionedArgs.fst;
   
-  local isF :: IOVal<Boolean> = isFile(fileName, ioIn);
-
-  local cppCmd :: String = "gcc -E -x c -D _POSIX_C_SOURCE -std=gnu1x -I . " ++
-    cppOptions;
-  local cppOptions :: String =
-    if length(args) >= 2 then implode(" ", cppArgs) else "" ;
-
-  -- Run C pre processor over the file.
+  local cppOptions :: String = if length(args) >= 2 then implode(" ", cppArgs) else "" ;
+  local cppCmd :: String = "gcc -E -x c -D _POSIX_C_SOURCE -std=gnu1x -I . " ++ cppOptions;
   local fullCppCmd :: String = cppCmd ++ " \"" ++ fileName ++ "\" > " ++ cppFileName;
-  local mkCppFile :: IOVal<Integer> =
-    system(
-      fullCppCmd,  
-      if containsBy(stringEq, "--show-cpp", args)
-      then print("CPP command: " ++ fullCppCmd ++ "\n", isF.io)
-      else isF.io);
-
-  -- Read the output of CPP and parse it.
-  local text :: IOVal<String> = readFile(cppFileName, mkCppFile.io);
-
-  local result :: ParseResult<cst:Root> = theParser(text.iovalue, cppFileName);
-
-  local ast :: abs:Root = result.parseTree.ast;
-  ast.env = addEnv( map(xcArgDef, xcArgs) , emptyEnv() );
-
-  local hostAst :: abs:Root = ast.abs:host;
-  hostAst.env = ast.env;
   
-  local liftedAst :: abs:Root = hostAst.abs:lifted;
-  liftedAst.env = hostAst.env;
+  local result::IOMonad<Integer> = do (bindIO, returnIO) {
+    if null(args) then {
+      printM("Usage: [ableC invocation] [file name] [c preprocessor arguments]\n");
+      return 5;
+    } else {
+      isF::Boolean <- isFileM(fileName);
+      if !isF then {
+        printM("File \"" ++ fileName ++ "\" not found.\n");
+        return 1;
+      } else {
+        if containsBy(stringEq, "--show-cpp", args) then
+          printM("CPP command: " ++ fullCppCmd ++ "\n");
+        mkCppFile::Integer <-
+          systemM(
+          fullCppCmd);
+        if mkCppFile != 0 then {
+          printM("CPP call failed.\n");
+          return 3;
+        } else {
+          text :: String <- readFileM(cppFileName);
+          result :: ParseResult<cst:Root> = theParser(text, cppFileName);
+          if !result.parseSuccess then {
+            printM(result.parseErrors ++ "\n");
+            return 2;
+          } else {
+            ast :: Decorated abs:Root =
+              decorate result.parseTree.ast with {
+                env = addEnv( map(xcArgDef, xcArgs) , emptyEnv() );
+              };
+            hostAst :: Decorated abs:Root = decorate ast.abs:host with { env = ast.env; };
+            liftedAst :: Decorated abs:Root = decorate ast.abs:lifted with { env = ast.env; };
+            if containsBy(stringEq, "--show-ast", args) then {
+              printM(substitute("edu:umn:cs:melt:", "", hackUnparse(hostAst)) ++ "\n");
+              return 0;
+            }
+            else if containsBy(stringEq, "--show-host-ast", args) then {
+              printM(substitute("edu:umn:cs:melt:", "", hackUnparse(hostAst)) ++ "\n");
+              return 0;
+            }
+            else if containsBy(stringEq, "--show-lifted-ast", args) then {
+              printM(substitute("edu:umn:cs:melt:", "", hackUnparse(liftedAst)) ++ "\n");
+              return 0;
+            }
+            else if containsBy(stringEq, "--show-pp", args) then {
+              printM(show(100, ast.pp) ++ "\n");
+              return 0;
+            }
+            else if containsBy(stringEq, "--show-host-pp", args) then {
+              printM(show(100, hostAst.pp) ++ "\n");
+              return 0;
+            }
+            else {
+              errors::[Message] = 
+                if !null(ast.errors)
+                then ast.errors
+                else {- if !null(hostAst.errors) -- host error checking dissabled for efficency reasons
+                then wrn(loc("", -1, -1, -1, -1, -1, -1),
+                         "Errors in host tree:") :: hostAst.errors
+                else-} if !null(liftedAst.errors)
+                then wrn(loc("", -1, -1, -1, -1, -1, -1),
+                        "Errors in lifted tree:") :: liftedAst.errors
+                else if !null(liftedAst.abs:globalDecls)
+                then [wrn(loc("Top level", -1, -1, -1, -1, -1, -1),
+                          "globalDecls at top level in lifted tree: " ++ implode(", ", map(fst, liftedAst.abs:globalDecls)))]
+                else [];
+              if !null(errors) then
+                printM(messagesToString(errors) ++ "\n");
+              if containsBy(stringEq, "--force-trans", args) || null(errors) then
+                writeFileM(ppFileName, show(80, liftedAst.pp));
+              if containsErrors(errors, false) then
+                return 4;
+              else
+                return 0;
+            }
+          }
+        }
+      }
+    }
+  };
   
-  local errors::[Message] = 
-    if !null(ast.errors)
-    then ast.errors
-    else {- if !null(hostAst.errors) -- host error checking dissabled for efficency reasons
-    then wrn(loc("", -1, -1, -1, -1, -1, -1),
-             "Errors in host tree:") :: hostAst.errors
-    else-} if !null(liftedAst.errors)
-    then wrn(loc("", -1, -1, -1, -1, -1, -1),
-             "Errors in lifted tree:") :: liftedAst.errors
-    else if !null(liftedAst.abs:globalDecls)
-    then [wrn(loc("Top level", -1, -1, -1, -1, -1, -1),
-              "globalDecls at top level in lifted tree: " ++ implode(", ", map(fst, liftedAst.abs:globalDecls)))]
-    else [];
-
-  local writePP :: IO = writeFile(ppFileName, show(80, liftedAst.pp), text.io);
-
-  return if null(args) then
-    ioval(print("Usage: [ableC invocation] [file name] [c preprocessor arguments]\n", ioIn), 5)
-  else if !isF.iovalue then
-    ioval(print("File \"" ++ fileName ++ "\" not found.\n", isF.io), 1)
-  else if mkCppFile.iovalue != 0 then
-    ioval(print("CPP call failed.\n", mkCppFile.io), 3)
-  else if !result.parseSuccess then
-    ioval(print(result.parseErrors ++ "\n", text.io), 2)
-  else if containsBy(stringEq, "--show-ast", args) then
-    ioval(print(substitute("edu:umn:cs:melt:", "", hackUnparse(ast)) ++ "\n", text.io), 0)
-  else if containsBy(stringEq, "--show-host-ast", args) then
-    ioval(print(substitute("edu:umn:cs:melt:", "", hackUnparse(hostAst)) ++ "\n", text.io), 0)
-  else if containsBy(stringEq, "--show-lifted-ast", args) then
-    ioval(print(substitute("edu:umn:cs:melt:", "", hackUnparse(liftedAst)) ++ "\n", text.io), 0)
-  else if containsBy(stringEq, "--show-pp", args) then
-    ioval(print(show(100, ast.pp) ++ "\n", text.io), 0)
-  else if containsBy(stringEq, "--show-host-pp", args) then
-    ioval(print(show(100, hostAst.pp) ++ "\n", text.io), 0)
-  else if !null(errors) && (containsBy(stringEq, "--force-trans", args) || !containsErrors(errors, false)) then
-    ioval(print(messagesToString(errors) ++ "\n", writePP), if containsErrors(errors, false) then 4 else 0)
-  else if !null(errors) then
-    ioval(print(messagesToString(errors) ++ "\n", text.io), 4)
-  else
-    ioval(writePP, 0);
+  return evalIO(result, ioIn);
 }
 
 
