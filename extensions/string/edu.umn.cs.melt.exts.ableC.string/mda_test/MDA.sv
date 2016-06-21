fe6438ca5b2748b090b00c10ec40b175fc305612
Lucas Kramer <krame505@umn.edu>
2016-06-21 13:44:56 -0500

    Bug fix with incorrect mda grammar name

--- extensions/string/edu.umn.cs.melt.exts.ableC.string/mda_test/MDA.sv
+++ extensions/string/edu.umn.cs.melt.exts.ableC.string/mda_test/MDA.sv
@@ -3,7 +3,7 @@ grammar edu:umn:cs:melt:exts:ableC:string:mda_test;
 import edu:umn:cs:melt:ableC:host;
 
 copper_mda testLambdaExpr(ablecParser) {
-  edu:umn:cs:melt:exts:ableC:string:concretesyntax:tostring;
+  edu:umn:cs:melt:exts:ableC:string:concretesyntax:str;
 }
 
 copper_mda testLambdaExpr(ablecParser) {
