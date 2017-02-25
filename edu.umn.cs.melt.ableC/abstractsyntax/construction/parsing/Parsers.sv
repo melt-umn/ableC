c4d3afd8a04e05b906ea9805d637c3cab22185d4
Lucas Kramer <krame505@umn.edu>
2017-02-25 16:30:02 -0600

    Fixes to parsing, added mechanism to specify typedef protos for lexer hack

--- edu.umn.cs.melt.ableC/abstractsyntax/construction/parsing/Parsers.sv
+++ edu.umn.cs.melt.ableC/abstractsyntax/construction/parsing/Parsers.sv
@@ -3,26 +3,32 @@ grammar edu:umn:cs:melt:ableC:abstractsyntax:construction:parsing;
 imports silver:langutil;
 
 imports edu:umn:cs:melt:ableC:concretesyntax as cst;
+imports edu:umn:cs:melt:ableC:concretesyntax:lexerHack as lh;
 imports edu:umn:cs:melt:ableC:abstractsyntax:construction;
-imports edu:umn:cs:melt:ableC:abstractsyntax:substitution;
 imports edu:umn:cs:melt:ableC:abstractsyntax;
 
+-- Parser declarations
 parser declsParser :: cst:TranslationUnit_c {
   edu:umn:cs:melt:ableC:concretesyntax;
+  edu:umn:cs:melt:ableC:concretesyntax:construction;
 }
 
 parser declParser :: cst:ExternalDeclaration_c {
   edu:umn:cs:melt:ableC:concretesyntax;
+  edu:umn:cs:melt:ableC:concretesyntax:construction;
 }
 
-parser stmtParser :: cst:CompoundStatement_c {
+parser stmtParser :: cst:BlockItemList_c {
   edu:umn:cs:melt:ableC:concretesyntax;
+  edu:umn:cs:melt:ableC:concretesyntax:construction;
 }
 
 parser exprParser :: cst:Expr_c {
   edu:umn:cs:melt:ableC:concretesyntax;
+  edu:umn:cs:melt:ableC:concretesyntax:construction;
 }
 
+-- Wrapper functions to call parsers and return asts
 function parseDecls
 Decls ::= text::String
 {
@@ -48,11 +54,11 @@ Decl ::= text::String
 function parseStmt
 Stmt ::= text::String
 {
-  local result::ParseResult<cst:CompoundStatement_c> =
+  local result::ParseResult<cst:BlockItemList_c> =
     stmtParser(text, s"parseStmt(\"\"\"\n${foldLineNums(text)}\n\"\"\")");
   return
     if result.parseSuccess
-    then result.parseTree.ast
+    then foldStmt(result.parseTree.ast)
     else error("Syntax errors in parseStmt string:\n" ++ result.parseErrors);
 }
 
