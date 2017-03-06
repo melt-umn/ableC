873fa9464b9abc095fac61d4dfcff6a20a13bbaa
Lucas Kramer <krame505@umn.edu>
2017-03-06 08:22:41 -0600

    Adding using decl concrete syntax for real this time

---
+++ extensions/templating/edu.umn.cs.melt.exts.ableC.templating/concretesyntax/usingDecl/Decl.sv
@@ -0,0 +1,27 @@
+grammar edu:umn:cs:melt:exts:ableC:templating:concretesyntax:usingDecl;
+
+imports silver:langutil;
+
+imports edu:umn:cs:melt:ableC:concretesyntax;
+imports edu:umn:cs:melt:ableC:concretesyntax:lexerHack as lh;
+
+imports edu:umn:cs:melt:ableC:abstractsyntax as ast;
+imports edu:umn:cs:melt:ableC:abstractsyntax:construction as ast;
+
+imports edu:umn:cs:melt:exts:ableC:templating:abstractsyntax;
+imports edu:umn:cs:melt:exts:ableC:templating:concretesyntax:templateDecl;
+
+-- Spurious import, to trigger the tests on build.
+import edu:umn:cs:melt:exts:ableC:templating:mda_test;
+
+marking terminal Using_t 'using' lexer classes {Ckeyword};
+
+concrete production usingDeclaration_c
+top::ExternalDeclaration_c ::= 'using' id::Identifier_t '<' params::TemplateParams_c '>' '=' ty::TypeName_c ';'
+{
+  top.ast = templateTypeDecl(params.ast, ast:fromId(id), ty.ast);
+}
+action {
+  context = lh:closeScope(context); -- Opened by TemplateDecl_c
+  context = lh:addTypenamesToScope([ast:fromId(id)], context);
+}
