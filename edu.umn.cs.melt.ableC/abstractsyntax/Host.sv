c29f909ada4988f4d8694e07380220ed577805a3
Lucas Kramer <krame505@umn.edu>
2017-03-31 13:50:54 -0500

    Revert "defsDecl gets removed by host instead of lifted"
    This reverts commit 8d5f388bda6f4ee52ea1e075893fc3c01411ab59.

--- edu.umn.cs.melt.ableC/abstractsyntax/Host.sv
+++ edu.umn.cs.melt.ableC/abstractsyntax/Host.sv
@@ -3,9 +3,7 @@ grammar edu:umn:cs:melt:ableC:abstractsyntax;
 {--
  - Functor attribute to compute the host AST corresponding to an extended AST
  - Invariant:
- - All forwarding (extension) productions should leave host undefined
  - All non-forwarding (host) productions should functor-propagate host
- - The exception to this are 'injection' productions such as defsDecl that modify host attributes,
- - but shouldn't really be considered part of the host tree.  
+ - All forwarding (extension) productions should leave it undefined
  -}
 synthesized attribute host<a>::a;
