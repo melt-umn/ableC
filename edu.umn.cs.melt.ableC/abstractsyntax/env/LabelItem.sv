e599ee4267378082135bf00f3ee7ee787bed31c3
Lucas Kramer <krame505@umn.edu>
2018-02-08 21:45:01 -0500

    Add a comment

--- edu.umn.cs.melt.ableC/abstractsyntax/env/LabelItem.sv
+++ edu.umn.cs.melt.ableC/abstractsyntax/env/LabelItem.sv
@@ -2,6 +2,9 @@ grammar edu:umn:cs:melt:ableC:abstractsyntax:env;
 
 nonterminal LabelItem with sourceLocation;
 
+-- Note that we unfortunately can't include a reference to the labeled statement here, since these
+-- are function-scope defs, which need to be computed without the use of env to avoid a circular
+-- dependancy.
 abstract production labelItem
 top::LabelItem ::= sourceLocation::Location
 {
