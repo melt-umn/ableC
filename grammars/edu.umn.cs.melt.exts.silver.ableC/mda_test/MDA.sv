6583f88deb5ddd10d3514d7513b6a3ca7d52f924
Lucas Kramer <krame505@umn.edu>
2018-05-09 15:51:40 -0500

    Adding MDA test

---
+++ grammars/edu.umn.cs.melt.exts.silver.ableC/mda_test/MDA.sv
@@ -0,0 +1,7 @@
+grammar edu:umn:cs:melt:exts:silver:ableC:mda_test;
+
+import silver:composed:Default;
+
+copper_mda testAbleCEmbed(svParse) {
+  edu:umn:cs:melt:exts:silver:ableC:concretesyntax;
+}
