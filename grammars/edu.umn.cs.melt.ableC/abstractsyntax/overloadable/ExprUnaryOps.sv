37d33dfed67fa695de35b19db913368273851181
Lucas Kramer <krame505@umn.edu>
2018-11-21 20:06:35 -0600

    Environment and performance-related fixes on overload productions

--- grammars/edu.umn.cs.melt.ableC/abstractsyntax/overloadable/ExprUnaryOps.sv
+++ grammars/edu.umn.cs.melt.ableC/abstractsyntax/overloadable/ExprUnaryOps.sv
@@ -7,7 +7,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:preIncExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:preIncExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.preIncProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -23,7 +24,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:preDecExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:preDecExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.preDecProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -39,7 +41,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:postIncExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:postIncExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.postIncProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -55,7 +58,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:postDecExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:postDecExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.postDecProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -71,7 +75,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:addressOfExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:addressOfExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.addressOfProd of
       just(prod) -> host:transformedExpr(host, prod(top.location), location=top.location)
@@ -87,7 +92,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:dereferenceExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:dereferenceExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.dereferenceProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -103,7 +109,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:positiveExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:positiveExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.positiveProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -119,7 +126,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:negativeExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:negativeExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.negativeProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -135,7 +143,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:bitNegateExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:bitNegateExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.bitNegateProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
@@ -151,7 +160,8 @@ top::host:Expr ::= e::host:Expr
   production attribute lerrors :: [Message] with ++;
   lerrors := case top.env, top.host:returnType of emptyEnv_i(), nothing() -> [] | _, _ -> [] end;
 
-  local host::host:Expr = inj:notExpr(e, location=top.location);
+  local host::host:Expr =
+    inj:notExpr(host:decExpr(e, location=e.location), location=top.location);
   local fwrd::host:Expr =
     case e.host:typerep.notProd of
       just(prod) -> host:transformedExpr(host, prod(e, top.location), location=top.location)
