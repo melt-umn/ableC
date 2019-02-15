0f1f63b3b8e85c906344e01e90849527712f5e55
Lucas Kramer <krame505@umn.edu>
2019-02-15 13:14:41 -0600

    Show entire location in redeclaration errors (instead of just line numbers)

--- grammars/edu.umn.cs.melt.ableC/abstractsyntax/host/Name.sv
+++ grammars/edu.umn.cs.melt.ableC/abstractsyntax/host/Name.sv
@@ -173,8 +173,8 @@ function doValueRedeclarationCheck
             herePP :: String = show(100, cat(t.lpp, t.rpp))
          in
             [err(n.location, 
-              "Redeclaration of " ++ n.name ++ " with incompatible types. Original (from line " ++
-              toString(v.sourceLocation.line) ++ ") " ++ originalPP ++ 
+              "Redeclaration of " ++ n.name ++ " with incompatible types. Original (from " ++
+              v.sourceLocation.unparse ++ ") " ++ originalPP ++
               " but here it is " ++ herePP)]
         end
   end;
@@ -187,8 +187,8 @@ function doValueRedeclarationCheckNoCompatible
   | [] -> []
   | v :: _ -> 
       [err(n.location, 
-        "Redeclaration of " ++ n.name ++ ". Original (from line " ++
-        toString(v.sourceLocation.line) ++ ")")]
+        "Redeclaration of " ++ n.name ++ ". Original (from " ++
+        v.sourceLocation.unparse ++ ")")]
   end;
 }
 
