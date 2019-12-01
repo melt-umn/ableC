cd4685b55b9459ee34408ac006f178c2e98632e9
Lucas Kramer <krame505@umn.edu>
2019-11-30 19:09:39 -0600

    Move tutorials to external repo

--- tutorials/embedded_dsl/edu.umn.cs.melt.tutorials.ableC.prefixExpr/Export.sv
+++
@@ -1,4 +0,0 @@
-grammar edu:umn:cs:melt:tutorials:ableC:prefixExpr;
-
-exports edu:umn:cs:melt:tutorials:ableC:prefixExpr:abstractsyntax;
-exports edu:umn:cs:melt:tutorials:ableC:prefixExpr:concretesyntax;
--- tutorials/extended_env/edu.umn.cs.melt.tutorials.ableC.intconst/Export.sv
+++
@@ -1,4 +0,0 @@
-grammar edu:umn:cs:melt:tutorials:ableC:intconst;
-
-exports edu:umn:cs:melt:tutorials:ableC:intconst:abstractsyntax;
-exports edu:umn:cs:melt:tutorials:ableC:intconst:concretesyntax;
