b63c986eb66a096c7ce6a54a6aecf50192f7e89c
Lucas Kramer <krame505@umn.edu>
2019-12-09 18:41:59 -0600

    Add mechanism for disambiguating between Global terminals

--- grammars/edu.umn.cs.melt.ableC/concretesyntax/LexerHack.sv
+++ grammars/edu.umn.cs.melt.ableC/concretesyntax/LexerHack.sv
@@ -45,7 +45,9 @@ lexer class Scoped
     
     {- In order of preference:
      - * Looked-up terminal from context
-     - * Any valid terminal from Global lexer class
+     - * Any valid, unambigous terminal from Global lexer class
+     -   * If there are more than one, check the globalPrerences parser attribute
+     -   * Otherwise, fail
      - * Identifier_t, if valid
      - * TypeName_t, if valid
      - * Any (arbitrary) thing that is valid: if the parse succeeds there will
@@ -55,13 +57,17 @@ lexer class Scoped
       case lookupResult, intersectBy(terminalIdEq, shiftable, Global) of
       | just(id), _ -> id
       | nothing(), [id] -> id
-      | nothing(), _ :: _ -> disambiguationFailure
       | nothing(), [] ->
         if containsBy(terminalIdEq, Identifier_t, shiftable)
         then Identifier_t
         else if containsBy(terminalIdEq, TypeName_t, shiftable)
         then TypeName_t
         else head(shiftable) -- Always has length >= 2
+      | nothing(), ids ->
+        case lookupBy(terminalSetEq, ids, globalPreferences) of
+        | just(id) -> id
+        | nothing() -> disambiguationFailure
+        end
       end;
   };
 
@@ -70,6 +76,14 @@ lexer class Scoped
 -- Extensions should use this class rather than Reserved, as marking terminals should not
 -- use lexical precedence.
 lexer class Global extends Scoped;
+
+-- Lexical ambiguities between Global terminals cannot usually be decided by the Scoped
+-- disambuguation class, and must ordinarily be resolved at composition time with
+-- transparent prefixes or explicit disambiguation groups.
+-- Extensions may occasionally introduce such ambiguities internally.  Instead of a
+-- disambiguation group, they can specify a preference using the globalPreferences parser attribute.
+parser attribute globalPreferences::[Pair<[TerminalId] TerminalId>]
+  action { globalPreferences = []; };
 
 -- The logic that mutates the 'context' value is distributed amoung the rules
 -- elsewhere in the grammar. grep for 'action' to find them all.
