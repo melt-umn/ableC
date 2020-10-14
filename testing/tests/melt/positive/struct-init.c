87c9cf272a4841b67bfd90804d91e0b3826e5262
Lucas Kramer <krame505@umn.edu>
2020-10-13 23:46:41 -0500

    Adding more tests for struct initialzation

---
+++ testing/tests/melt/positive/struct-init.c
@@ -0,0 +1,33 @@
+struct __pthread_cond_s
+{
+  union
+  {
+    unsigned long long int __wseq;
+    struct
+    {
+      unsigned int __low;
+      unsigned int __high;
+    } __wseq32;
+  };
+  union
+  {
+    unsigned long long int __g1_start;
+    struct
+    {
+      unsigned int __low;
+      unsigned int __high;
+    } __g1_start32;
+  };
+  unsigned int __g_refs[2] ;
+  unsigned int __g_size[2];
+  unsigned int __g1_orig_size;
+  unsigned int __wrefs;
+  unsigned int __g_signals[2];
+};
+
+int main() {
+  struct __pthread_cond_s c1 = { {0}, {0}, {0, 0}, {0, 0}, 0, 0, {0, 0} };
+  struct __pthread_cond_s c2 = { {.__wseq = 42} };
+  struct __pthread_cond_s c3 = { .__wseq = 42 };
+  struct __pthread_cond_s c4 = { 43 };
+}
