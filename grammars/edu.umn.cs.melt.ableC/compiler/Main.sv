d8a9376e428035697c3644436e0a44227ac0d815
Lucas Kramer <krame505@umn.edu>
2025-06-24 15:07:46 -0500

    Main driver for use with codeprober

--- grammars/edu.umn.cs.melt.ableC/compiler/Main.sv
+++ grammars/edu.umn.cs.melt.ableC/compiler/Main.sv
@@ -1,10 +1,14 @@
 grammar edu:umn:cs:melt:ableC:compiler;
 
 import edu:umn:cs:melt:ableC:host;
+import edu:umn:cs:melt:ableC:abstractsyntax:host;
 import edu:umn:cs:melt:ableC:drivers:compile;
+import edu:umn:cs:melt:ableC:drivers:codeProber as cpr;
 
 -- Not used here, but included in the artifact
 import edu:umn:cs:melt:ableC:silverconstruction only;
 
 fun main IO<Integer> ::= args::[String] = driver(args, ablecParser);
 
+fun codeProberParse IO<Decorated Compilation> ::= args::[String] =
+  cpr:driver(args, ablecParser);
